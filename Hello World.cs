128b6935fc66cb04221f41ea68a68397524c9f1f
SamSang <akapeterser@gmail.com>
2018-05-05 22:06:38 -0400

    sample change

---
+++ Hello World.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! - change 1");
+        }
+    }
+}
