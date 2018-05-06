fda61103d03defb477c703f3ce0bc74792f6522c
SamSang <akapeterser@gmail.com>
2018-05-05 23:29:21 -0400

    change 6

--- Hello World.cs
+++ Hello World.cs
@@ -6,7 +6,7 @@ namespace Lenie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! - change 1");
+            Console.WriteLine("Hello World! - change 6");
         }
     }
 }
